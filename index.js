1467b3546c2c0fddb974bc2234e3c083753b7409
vresidarauch <vresidarauch@yahoo.com>
2017-10-09 18:31:33 -0400

    Initial
    Lightbox

---
+++ index.js
@@ -0,0 +1,8 @@
+model = [
+  {
+    imgPath: 'photos/01.jpg',
+    title: 'Hay Bales',
+    caption:
+      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+  }
+];
